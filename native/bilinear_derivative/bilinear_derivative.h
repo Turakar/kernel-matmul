0dbd96f40a6edcbcd5c6a491daafaa3c26c8abf0
Turakar <turakar23@gmail.com>
2023-12-06 17:01:30 +0000

    Add #define checks

--- native/bilinear_derivative/bilinear_derivative.h
+++ native/bilinear_derivative/bilinear_derivative.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#ifndef KM_BLOCK_SIZE
+#error "KM_BLOCK_SIZE must be defined"
+#endif
+
+#ifndef KM_BILINEAR_DERIVATIVE_THREAD_DIM
+#error "KM_BILINEAR_DERIVATIVE_THREAD_DIM must be defined"
+#endif
+
+#ifndef KM_BILINEAR_DERIVATIVE_PER_THREAD
+#error "KM_BILINEAR_DERIVATIVE_PER_THREAD must be defined"
+#endif
+
 #include <torch/extension.h>
 
 torch::Tensor kernel_bilinear_derivative_cuda(torch::Tensor x1, torch::Tensor x2,
